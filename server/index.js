2b3ee9eae5e7a62c87c9546ab3b408b66a22ebf3
Code-with-Viv <vyvivek2018@gmail.com>
2023-03-17 16:57:45 +0530

    Server established

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(express.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
 
+
+  
 
 app.listen(port,()=>{
     console.log(`Server has started on port ${port}`);
