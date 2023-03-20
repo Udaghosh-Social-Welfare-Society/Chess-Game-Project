eab414f68d0f6773af7f1df1fe6f7447f81c7fa2
suraj0798 <surajdhas07@gmail.com>
2023-03-20 16:10:25 +0530

    database connection

---
+++ server/database.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const dataBase = process.env.ATLAS_URI;
+
+// Connection To The Database
+const dbConnection = async () => {
+    try {
+        await mongoose.connect(dataBase, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        });
+        console.log('Connected to database');
+    } catch (err) {
+        console.log('Error while connecting with database')
+        console.log(err);
+    }
+};
+
+module.exports = dbConnection;
+
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const dbConnection = require ("./database.js")
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -9,8 +10,7 @@ app.use(bodyParser.urlencoded({
     extended: true
   }));
 
-
-  
+dbConnection();
 
 app.listen(port,()=>{
     console.log(`Server has started on port ${port}`);
